b9706a6bc961309bda52d417ed9eb8d44374b1d8
SonlyF5020 <502089948@163.com>
2013-08-01 09:04:24 +0800

    change js structure

--- src/main/webapp/resources/js/home.js
+++ src/main/webapp/resources/js/home.js
@@ -1,6 +1,52 @@
 var deleteContent = "";
 
 $(function () {
+    getHistory();
+    getUserName();
+
+    $('#confirm').bind("click", function () {
+        var year = new Date().getYear().toString().substring(1, 3);
+        var month = (new Date().getMonth() + 1).toString();
+        var date = new Date().getDate().toString();
+        var dateString = "---20" + year + "." + month + "." + date;
+        $('#newContent').val($('#weibo').val());
+        $('#newContentDate').val(dateString);
+        $('#weibo').val("");
+        $('#contentSubmit').click();
+    });
+
+    $('#weiboContent').on('mouseover','.oneContent',function(){
+        $(this).addClass('mouseOver');
+        if($('span',this).html()===$('#currentUserName').html()){
+            $('.deleteButton', this).show();
+        }
+        $('.writeButton', this).show();
+    });
+
+    $('#weiboContent').on('mouseout','.oneContent',function(){
+        $('.oneContent.mouseOver').removeClass('mouseOver');
+        $('.deleteButton').hide();
+        $('.writeButton').hide();
+    });
+
+    $('#weiboContent').on('click','.deleteButton',function(){
+        $('#myModal').modal('show');
+        deleteContent = $(this).parent().attr("id");
+    });
+
+    $('#myModal').on('click','#sureButton',function(){
+        document.location.href='/delete?deleteIndex='+deleteContent
+        $('#myModal').modal('hide');
+    });
+});
+
+var getUserName = function(){
+    $.getJSON("/json/currentUserName",function(allData){
+        $('#currentUserName').html(allData["userName"]);
+    });
+}
+
+var getHistory = function(){
     $.getJSON("/json/userHistory",function(allData){
         var index;
         for(index in allData){
@@ -12,43 +58,5 @@ $(function () {
             $('#weiboContent').prepend(chart);
         }
     });
+}
 
-    $.getJSON("/json/currentUserName",function(allData){
-        $('#currentUserName').html(allData["userName"]);
-    });
-
-    $('#confirm').live("click", function () {
-        var year = new Date().getYear().toString().substring(1, 3);
-        var month = (new Date().getMonth() + 1).toString();
-        var date = new Date().getDate().toString();
-        var dateString = "---20" + year + "." + month + "." + date;
-        $('#newContent').val($('#weibo').val());
-        $('#newContentDate').val(dateString);
-        $('#weibo').val("");
-        $('#contentSubmit').click();
-    });
-
-    $('.oneContent').live('mouseover', function () {
-        $(this).addClass('mouseOver');
-        if($('span',this).html()===$('#currentUserName').html()){
-            $('.deleteButton', this).show();
-        }
-        $('.writeButton', this).show();
-    });
-
-    $('.oneContent').live('mouseout', function () {
-        $('.oneContent').removeClass('mouseOver');
-        $('.deleteButton').hide();
-        $('.writeButton').hide();
-    });
-
-    $('.deleteButton').live('click', function () {
-        $('#myModal').modal('show');
-        deleteContent = $(this).parent().attr("id");
-    });
-
-    $('#sureButton').live('click', function () {
-        document.location.href='/delete?deleteIndex='+deleteContent
-        $('#myModal').modal('hide');
-    });
-});
