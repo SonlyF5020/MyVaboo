3e20f36dd40797526b480f471538d3877640e8ed
SonlyF5020 <502089948@163.com>
2013-09-01 22:52:40 +0800

    MD5 is interesting

--- src/main/java/com/thoughtworks/zhanhonglai/data/UserInfo.java
+++ src/main/java/com/thoughtworks/zhanhonglai/data/UserInfo.java
@@ -1,5 +1,9 @@
 package com.thoughtworks.zhanhonglai.data;
 
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class UserInfo {
     private String UserName;
     private String password;
@@ -25,7 +29,30 @@ public class UserInfo {
         UserName = userName;
     }
 
-    public void setPassword(String password) {
+    public void setPassword(String password){
+        MessageDigest messageDigest = null;
+        try {
+            messageDigest = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        messageDigest.update(password.getBytes());
+
+        byte[] bs=messageDigest.digest();   //进行加密运算并返回字符数组
+        for(byte b: bs){
+            System.out.print(b+"|"); //打印出来加密后的字符数组
+        }
+        System.out.println();
+
+        StringBuffer sb=new StringBuffer();
+        for(int i=0;i<bs.length;i++){    //字节数组转换成十六进制字符串，形成最终的密文
+            int v=bs[i]&0xff;
+            if(v<16){
+                sb.append(0);
+            }
+            sb.append(Integer.toHexString(v));
+        }
+        System.out.println("加密之后的字符串："+sb);
         this.password = password;
     }
 
@@ -37,7 +64,7 @@ public class UserInfo {
         this.emailAddress = emailAddress;
     }
 
-    public UserInfo(String userName, String password, String userFaceUrl) {
+    public UserInfo(String userName, String password, String userFaceUrl){
         setUserName(userName);
         setPassword(password);
         setUserFaceUrl(userFaceUrl);
---
+++ src/test/java/com/thoughtworks/zhanhonglai/HashTest.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.zhanhonglai;
+
+import com.thoughtworks.zhanhonglai.data.UserInfo;
+import org.junit.Test;
+
+public class HashTest {
+    @Test
+    public void test() throws Exception {
+        UserInfo userInfo = new UserInfo();
+        userInfo.setPassword("a");
+    }
+}
